65e5124b352abc9927c00cc9508d6a28fb8a7a6a
steve <steve@b9360e03-a0fa-4602-804e-483810d782e8>
2009-09-12 22:41:44 +0000

    Version 0.203 Added tone burst detection for our European friends.

--- asterisk/apps/app_rpt.c
+++ asterisk/apps/app_rpt.c
@@ -21,7 +21,7 @@
 /*! \file
  *
  * \brief Radio Repeater / Remote Base program 
- *  version 0.202 9/4/2009 
+ *  version 0.203 9/12/2009 
  * 
  * \author Jim Dixon, WB6NIL <jim@lambdatel.com>
  *
@@ -261,6 +261,12 @@
 #define	DEFAULT_IRLPANN 1
 #define	DEFAULT_EANNMODE 1
 
+#define	DEFAULT_RXBURST_TIME 250 
+#define	DEFAULT_RXBURST_THRESHOLD 16
+
+#define TONE_SAMPLE_RATE 8000
+#define TONE_SAMPLES_IN_FRAME 160
+
 #define	NODES "nodes"
 #define	EXTNODES "extnodes"
 #define MEMORY "memory"
@@ -459,7 +465,7 @@ int ast_playtones_start(struct ast_channel *chan, int vol, const char* tonelist,
 /*! Stop the tones from playing */
 void ast_playtones_stop(struct ast_channel *chan);
 
-static  char *tdesc = "Radio Repeater / Remote Base  version 0.202  9/4/2009";
+static  char *tdesc = "Radio Repeater / Remote Base  version 0.203  9/12/2009";
 
 static char *app = "Rpt";
 
@@ -825,6 +831,37 @@ struct rpt_cmd_struct
     int command_source;
 };
 
+typedef struct {
+	int v2;
+	int v3;
+	int chunky;
+	int fac;
+	int samples;
+} goertzel_state_t;
+
+typedef struct {
+	int value;
+	int power;
+} goertzel_result_t;
+
+typedef struct
+{
+	int freq;
+	int block_size;
+	int squelch;		/* Remove (squelch) tone */
+	goertzel_state_t tone;
+	float energy;		/* Accumulated energy of the current block */
+	int samples_pending;	/* Samples remain to complete the current block */
+	int mute_samples;	/* How many additional samples needs to be muted to suppress already detected tone */
+
+	int hits_required;	/* How many successive blocks with tone we are looking for */
+	float threshold;	/* Energy of the tone relative to energy from all other signals to consider a hit */
+
+	int hit_count;		/* How many successive blocks we consider tone present */
+	int last_hit;		/* Indicates if the last processed block was a hit */
+
+} tone_detect_state_t;
+
 static struct rpt
 {
 	ast_mutex_t lock;
@@ -930,6 +967,9 @@ static struct rpt
 		char nounkeyct;
 		char holdofftelem;
 		char beaconing;
+		int rxburstfreq;
+		int rxbursttime;
+		int rxburstthreshold;
 	} p;
 	struct rpt_link links;
 	int unkeytocttimer;
@@ -1039,6 +1079,9 @@ static struct rpt
 	long rxlingertimer;
 	char localoverride;
 	char ready;
+	char lastrxburst;
+	char reallykeyed;
+	tone_detect_state_t burst_tone_state;
 #ifdef OLD_ASTERISK
 	AST_LIST_HEAD(, ast_frame) txq;
 #else
@@ -1534,6 +1577,195 @@ static struct telem_defaults tele_defs[] = {
 	{"cmdmode","|t(900,904,200,2048)"},
 	{"functcomplete","|t(1000,0,100,2048)(0,0,100,0)(1000,0,100,2048)"}
 } ;
+
+static inline void goertzel_sample(goertzel_state_t *s, short sample)
+{
+	int v1;
+	
+	v1 = s->v2;
+	s->v2 = s->v3;
+	
+	s->v3 = (s->fac * s->v2) >> 15;
+	s->v3 = s->v3 - v1 + (sample >> s->chunky);
+	if (abs(s->v3) > 32768) {
+		s->chunky++;
+		s->v3 = s->v3 >> 1;
+		s->v2 = s->v2 >> 1;
+		v1 = v1 >> 1;
+	}
+}
+
+static inline void goertzel_update(goertzel_state_t *s, short *samps, int count)
+{
+	int i;
+	
+	for (i = 0; i < count; i++) {
+		goertzel_sample(s, samps[i]);
+	}
+}
+
+
+static inline float goertzel_result(goertzel_state_t *s)
+{
+	goertzel_result_t r;
+	r.value = (s->v3 * s->v3) + (s->v2 * s->v2);
+	r.value -= ((s->v2 * s->v3) >> 15) * s->fac;
+	r.power = s->chunky * 2;
+	return (float)r.value * (float)(1 << r.power);
+}
+
+static inline void goertzel_init(goertzel_state_t *s, float freq, int samples)
+{
+	s->v2 = s->v3 = s->chunky = 0.0;
+	s->fac = (int)(32768.0 * 2.0 * cos(2.0 * M_PI * freq / TONE_SAMPLE_RATE));
+	s->samples = samples;
+}
+
+static inline void goertzel_reset(goertzel_state_t *s)
+{
+	s->v2 = s->v3 = s->chunky = 0.0;
+}
+
+static void tone_detect_init(tone_detect_state_t *s, int freq, int duration, int amp)
+{
+	int duration_samples;
+	float x;
+	int periods_in_block;
+
+	s->freq = freq;
+
+	/* Desired tone duration in samples */
+	duration_samples = duration * TONE_SAMPLE_RATE / 1000;
+	/* We want to allow 10% deviation of tone duration */
+	duration_samples = duration_samples * 9 / 10;
+
+	/* If we want to remove tone, it is important to have block size not
+	   to exceed frame size. Otherwise by the moment tone is detected it is too late
+ 	   to squelch it from previous frames */
+	s->block_size = TONE_SAMPLES_IN_FRAME;
+
+	periods_in_block = s->block_size * freq / TONE_SAMPLE_RATE;
+
+	/* Make sure we will have at least 5 periods at target frequency for analisys.
+	   This may make block larger than expected packet and will make squelching impossible
+	   but at least we will be detecting the tone */
+	if (periods_in_block < 5)
+		periods_in_block = 5;
+
+	/* Now calculate final block size. It will contain integer number of periods */
+	s->block_size = periods_in_block * TONE_SAMPLE_RATE / freq;
+
+	/* tone_detect is currently only used to detect fax tones and we
+	   do not need suqlching the fax tones */
+	s->squelch = 0;
+
+	/* Account for the first and the last block to be incomplete
+	   and thus no tone will be detected in them */
+	s->hits_required = (duration_samples - (s->block_size - 1)) / s->block_size;
+
+	goertzel_init(&s->tone, freq, s->block_size);
+
+	s->samples_pending = s->block_size;
+	s->hit_count = 0;
+	s->last_hit = 0;
+	s->energy = 0.0;
+
+	/* We want tone energy to be amp decibels above the rest of the signal (the noise).
+	   According to Parseval's theorem the energy computed in time domain equals to energy
+	   computed in frequency domain. So subtracting energy in the frequency domain (Goertzel result)
+	   from the energy in the time domain we will get energy of the remaining signal (without the tone
+	   we are detecting). We will be checking that
+		10*log(Ew / (Et - Ew)) > amp
+	   Calculate threshold so that we will be actually checking
+		Ew > Et * threshold
+	*/
+
+	x = pow(10.0, amp / 10.0);
+	s->threshold = x / (x + 1);
+
+	ast_log(LOG_DEBUG,"Setup tone %d Hz, %d ms, block_size=%d, hits_required=%d\n", freq, duration, s->block_size, s->hits_required);
+}
+
+
+static int tone_detect(tone_detect_state_t *s, int16_t *amp, int samples)
+{
+	float tone_energy;
+	int i;
+	int hit = 0;
+	int limit;
+	int res = 0;
+	int16_t *ptr;
+	int start, end;
+
+	for (start = 0;  start < samples;  start = end) {
+		/* Process in blocks. */
+		limit = samples - start;
+		if (limit > s->samples_pending) {
+			limit = s->samples_pending;
+		}
+		end = start + limit;
+
+		for (i = limit, ptr = amp ; i > 0; i--, ptr++) {
+			/* signed 32 bit int should be enough to suqare any possible signed 16 bit value */
+			s->energy += (int32_t) *ptr * (int32_t) *ptr;
+
+			goertzel_sample(&s->tone, *ptr);
+		}
+
+		s->samples_pending -= limit;
+
+		if (s->samples_pending) {
+			/* Finished incomplete (last) block */
+			break;
+		}
+
+		tone_energy = goertzel_result(&s->tone);
+
+		/* Scale to make comparable */
+		tone_energy *= 2.0;
+		s->energy *= s->block_size;
+
+		hit = 0;
+		ast_log(LOG_DEBUG,"tone %d, Ew=%.2E, Et=%.2E, s/n=%10.2f\n", s->freq, tone_energy, s->energy, tone_energy / (s->energy - tone_energy));
+		if (tone_energy > s->energy * s->threshold) {
+			ast_log(LOG_DEBUG,"Hit! count=%d\n", s->hit_count);
+			hit = 1;
+		}
+
+		if (s->hit_count) {
+			s->hit_count++;
+		}
+
+		if (hit == s->last_hit) {
+			if (!hit) {
+				/* Two successive misses. Tone ended */
+				s->hit_count = 0;
+			} else if (!s->hit_count) {
+				s->hit_count++;
+			}
+
+		}
+
+		if (s->hit_count >= s->hits_required) {
+			ast_log(LOG_DEBUG,"%d Hz tone detected\n", s->freq);
+			res = 1;
+		}
+
+		s->last_hit = hit;
+
+		/* Reinitialise the detector for the next block */
+		/* Reset for the next block */
+		goertzel_reset(&s->tone);
+
+		/* Advance to the next block */
+		s->energy = 0.0;
+		s->samples_pending = s->block_size;
+
+		amp += limit;
+	}
+
+	return res;
+}
 
 
 /*
@@ -4639,7 +4871,15 @@ static char *cs_keywords[] = {"rptena","rptdis","apena","apdis","lnkena","lnkdis
 	rpt_vars[n].p.holdofftelem = ast_true(val);
 	val = (char *) ast_variable_retrieve(cfg, this, "beaconing");
 	rpt_vars[n].p.beaconing = ast_true(val);
-
+	val = (char *) ast_variable_retrieve(cfg,this,"rxburstfreq");
+	if (val) rpt_vars[n].p.rxburstfreq = atoi(val);
+	else rpt_vars[n].p.rxburstfreq = 0;
+	val = (char *) ast_variable_retrieve(cfg,this,"rxbursttime");
+	if (val) rpt_vars[n].p.rxbursttime = atoi(val);
+	else rpt_vars[n].p.rxbursttime = DEFAULT_RXBURST_TIME;
+	val = (char *) ast_variable_retrieve(cfg,this,"rxburstthreshold");
+	if (val) rpt_vars[n].p.rxburstthreshold = atoi(val);
+	else rpt_vars[n].p.rxburstthreshold = DEFAULT_RXBURST_THRESHOLD;
 
 #ifdef	__RPT_NOTCH
 	val = (char *) ast_variable_retrieve(cfg,this,"rxnotch");
@@ -15274,6 +15514,11 @@ char tmpstr[300],lstr[MAXLINKLIST];
 	myrpt->wasvox = 0;
 	myrpt->linkactivityflag = 0;
 	myrpt->linkactivitytimer = 0;
+	if (myrpt->p.rxburstfreq)
+	{
+		tone_detect_init(&myrpt->burst_tone_state,myrpt->p.rxburstfreq,
+			myrpt->p.rxbursttime,myrpt->p.rxburstthreshold);
+	}
 	if (myrpt->p.startupmacro)
 	{
 		snprintf(myrpt->macrobuf,MAXMACRO - 1,"PPPP%s",myrpt->p.startupmacro);
@@ -16256,13 +16501,38 @@ char tmpstr[300],lstr[MAXLINKLIST];
 				short *sp;
 				int n;
 #endif
-
+				if (myrpt->p.rxburstfreq)
+				{
+					if ((!myrpt->reallykeyed) || myrpt->keyed)
+					{
+						myrpt->lastrxburst = 0;
+						goertzel_reset(&myrpt->burst_tone_state.tone);
+						myrpt->burst_tone_state.last_hit = 0;
+						myrpt->burst_tone_state.hit_count = 0;
+						myrpt->burst_tone_state.energy = 0.0;
+						
+					}
+					else
+					{					
+						i = tone_detect(&myrpt->burst_tone_state,f->data,f->samples);
+						ast_log(LOG_DEBUG,"Node %s got %d Hz Rx Burst\n",
+							myrpt->name,myrpt->p.rxburstfreq);
+						if ((!i) && myrpt->lastrxburst)
+						{
+							ast_log(LOG_DEBUG,"Node %s now keyed after Rx Burst\n",myrpt->name);
+							myrpt->linkactivitytimer = 0;
+							myrpt->keyed = 1;
+							time(&myrpt->lastkeyedtime);
+							myrpt->keyposttimer = KEYPOSTSHORTTIME;
+						}
+						myrpt->lastrxburst = i;
+					}
+				}
 				if ((!myrpt->localtx) && /* (!myrpt->p.linktolink) && */
 				    (!myrpt->localoverride))
 				{
 					memset(f->data,0,f->datalen);
 				}
-
 #ifdef	_MDC_DECODE_H_
 				sp = (short *) f->data;
 				/* convert block to unsigned char */
@@ -16308,6 +16578,7 @@ char tmpstr[300],lstr[MAXLINKLIST];
 				/* apply inbound filters, if any */
 				rpt_filter(myrpt,f->data,f->datalen / 2);
 #endif
+
 				if (ioctl(myrpt->zaprxchannel->fds[0], ZT_GETCONFMUTE, &ismuted) == -1)
 				{
 					ismuted = 0;
@@ -16382,10 +16653,14 @@ char tmpstr[300],lstr[MAXLINKLIST];
 					{
 						if (debug >= 6)
 							ast_log(LOG_NOTICE,"**** rx key\n"); 
-						myrpt->linkactivitytimer = 0;
-						myrpt->keyed = 1;
-						time(&myrpt->lastkeyedtime);
-						myrpt->keyposttimer = KEYPOSTSHORTTIME;
+						myrpt->reallykeyed = 1;
+						if (!myrpt->p.rxburstfreq)
+						{
+							myrpt->linkactivitytimer = 0;
+							myrpt->keyed = 1;
+							time(&myrpt->lastkeyedtime);
+							myrpt->keyposttimer = KEYPOSTSHORTTIME;
+						}
 					}
 					if (myrpt->p.archivedir)
 					{
@@ -16467,6 +16742,7 @@ char tmpstr[300],lstr[MAXLINKLIST];
 						}
 					}
 					send_link_pl(myrpt,"0");
+					myrpt->reallykeyed = 0;
 					myrpt->keyed = 0;
 					if ((myrpt->p.duplex > 1) && myrpt->localoverride)
 					{
