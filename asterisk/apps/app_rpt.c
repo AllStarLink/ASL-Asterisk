9e887ea5e7e822d7dd6aaa04dcb570d418e29837
jdixon <jdixon@b9360e03-a0fa-4602-804e-483810d782e8>
2011-05-14 22:56:16 +0000

    Fixed error in last app_rpt commit

--- asterisk/apps/app_rpt.c
+++ asterisk/apps/app_rpt.c
@@ -525,7 +525,7 @@ struct ast_flags config_flags = { CONFIG_FLAG_WITHCOMMENTS };
 
 /* Un-comment the following to include support decoding of MDC-1200 digital tone
    signalling protocol (using KA6SQG's GPL'ed implementation) */
-#include "mdc_decode.c"
+/* #include "mdc_decode.c" */
 
 /* Un-comment the following to include support encoding of MDC-1200 digital tone
    signalling protocol (using KA6SQG's GPL'ed implementation) */
