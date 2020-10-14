0ab308bd80f226fe1e62b737c8e25c86fd2e0370
Adam Paul <adamjpaul@gmail.com>
2020-10-14 16:01:51 -0400

    update dockerbuild.sh

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -10,7 +10,7 @@ echo $DIR
 echo $PDIR
 
 for A in $ARCHS; do
-	docker build -f Dockerfile.$A -t asl-asterisk_builder.$A $DIR
+	docker build -f $DIR/Dockerfile.$A -t asl-asterisk_builder.$A $DIR
 	docker run -v $PDIR:/src asl-asterisk_builder.$A
 	docker image rm --force asl-asterisk_builder.$A
 done
