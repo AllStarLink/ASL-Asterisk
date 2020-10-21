f5c4bb338d00edac0a8b0ff366f8f35fa3adad57
Adam Paul <adamjpaul@gmail.com>
2020-10-20 20:06:22 -0400

    more efficient builds

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -5,11 +5,24 @@ ARCHS="amd64 armhf"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PDIR=$(dirname $DIR)
-echo $DIR
-echo $PDIR
 
+#get number of 'all' packages
+ALLCOUNT=$(egrep "^Architecture: ?all" debian/control | wc -l)
+
+#get number of 'any'(non-any) packages
+ANYCOUNT=$(grep "^Architecture:" debian/control | egrep -v "^Architecture: ?all" | wc -l)
+
+#if 'any' = 0, only run for one arch (there are no arch specific packages)
+if [ "$ANYCOUNT" -eq "0" ] ; then
+  set -- $ARCHS
+  ARCHS=$1
+fi
+
+#run --build=any for following arch's after the first to prevent re-creating 'all' packages
+DPKG_BUILDOPTS="-b -uc -us"
 for A in $ARCHS; do
 	docker build -f $DIR/Dockerfile.$A -t asl-asterisk_builder.$A $DIR
-	docker run -v $PDIR:/src asl-asterisk_builder.$A
+	docker run -v $PDIR:/src asl-asterisk_builder.$A --env DPKG_BUILDOPTS="$DPKG_BUILDOPTS"
 	docker image rm --force asl-asterisk_builder.$A
+	DPKG_BUILDOPTS="--build=any -uc -us"
 done
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
+#get DPKG_BUILDOPTS from env var or use default
+OPTS=${DPKG_BUILDOPTS:-"-b -uc -us"}
+
 
 #if the debian changelong changed for asterisk in the last commit, then build the asterisk package
 cd /src
 if git diff --name-only HEAD HEAD~1 | grep -q asterisk/debian/changelog; then
   cd /src/asterisk
   ./bootstrap.sh && ./configure
-  debuild -b -uc -us
+  debuild "$OPTS"
 fi
 
 #if the debian changelong changed for allstar in the last commit, then build the allstar package
 cd /src
 if git diff --name-only HEAD HEAD~1 | grep -q allstar/debian/changelog; then
   cd /src/allstar
-  debuild -b -uc -us
+  debuild "$OPTS"
 fi
