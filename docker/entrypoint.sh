2ced934a7aabf5654b568e5bfb447ae58c9a9865
Adam Paul <adamjpaul@gmail.com>
2020-10-20 22:09:25 -0400

    entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 #get DPKG_BUILDOPTS from env var or use default
 OPTS=${DPKG_BUILDOPTS:-"-b -uc -us"}
 
+env
+
 for t in "$BUILD_TARGETS"; do
+  echo "$t"
   cd /src/$t
+  pwd
   if [ "$t" == "asterisk" ]; then
     ./bootstrap.sh && ./configure
   fi
