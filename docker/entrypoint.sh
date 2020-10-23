7d4ddffa652fb6a7a6907efba7bd7c1daae284f1
Adam Paul <adamjpaul@gmail.com>
2020-10-22 22:34:16 -0400

    remove Jenkinsfile, move debuild output to build dir

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,5 +14,12 @@ for t in "$BUILD_TARGETS"; do
     ./bootstrap.sh && ./configure
   fi
   debuild $OPTS
+  BASENAME=$(head -1 debian/changelog | sed 's/^\([^ ]*\) (\([0-9]*:\)\?\([^)]*\)).*/\1_\3/g')
+  cd ..
+  mkdir -p build/$BASENAME
+  mv *.deb build/$BASENAME
+  mv *.build build/$BASENAME
+  mv *.buildinfo build/$BASENAME
+  mv *.changes build/$BASENAME
 done
-chown -R user ..
+chown -R user .
