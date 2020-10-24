cec1ddf210ffb2030172eb6b30b2ce1b1c0b1815
Adam Paul <adamjpaul@gmail.com>
2020-10-24 16:40:23 -0400

    implement dockerized jenkins package builds

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+set -e
 
 ARCHS="amd64 armhf"
-ARCHS=amd64
-
+TARGETS="asterisk allstar"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PDIR=$(dirname $DIR)
-echo $DIR
-echo $PDIR
 
+#get the build targets
+cd $PDIR
+BUILD_TARGETS=""
+ANYCOUNT=0
+for t in $TARGETS; do
+  if git diff --name-only HEAD HEAD~1 | grep -q $t/debian/changelog; then
+    BUILD_TARGETS+="$t "
+    c=$(grep "^Architecture:" $t/debian/control | egrep -v "^Architecture: ?all" | wc -l)
+    ANYCOUNT=$((c+ANYCOUNT))
+  fi
+done
+BUILD_TARGETS=$(echo "$BUILD_TARGETS" | xargs)
+
+
+#if 'any' = 0, only run for one arch (there are no arch specific packages)
+if [ "$ANYCOUNT" -eq "0" ] ; then
+  set -- $ARCHS
+  ARCHS=$1
+fi
+
+#run --build=any for following arch's after the first to prevent re-creating 'all' packages
+DPKG_BUILDOPTS="-b -uc -us"
 for A in $ARCHS; do
-	docker build -f $DIR/Dockerfile.$A -t asl-asterisk_builder.$A $DIR
-	docker run -v $PDIR:/src asl-asterisk_builder.$A
-	docker image rm --force asl-asterisk_builder.$A
+       docker build -f $DIR/Dockerfile.$A -t asl-asterisk_builder.$A --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) $DIR
+       docker run -v $PDIR:/src -e DPKG_BUILDOPTS="$DPKG_BUILDOPTS" -e BUILD_TARGETS="$BUILD_TARGETS" -e FOO=bar asl-asterisk_builder.$A
+       docker image rm --force asl-asterisk_builder.$A
+       DPKG_BUILDOPTS="--build=any -uc -us"
 done
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+set -e
 
-# Clear out the /build and /release directory
-#rm -rf /release/*
+#get DPKG_BUILDOPTS from env var or use default
+OPTS=${DPKG_BUILDOPTS:-"-b -uc -us"}
 
-# Re-pull the repository
-#git fetch 
+env
 
-# Configure, make, make install
-cd /src/asterisk
-./bootstrap.sh && ./configure
-debuild -b -uc -us
-
-cd /src/allstar
-debuild -b -uc -us
+for t in "$BUILD_TARGETS"; do
+  echo "$t"
+  cd /src/$t
+  pwd
+  if [ "$t" == "asterisk" ]; then
+    ./bootstrap.sh && ./configure
+  fi
+  debuild $OPTS
+  BASENAME=$(head -1 debian/changelog | sed 's/^\([^ ]*\) (\([0-9]*:\)\?\([^)]*\)).*/\1_\3/g')
+  cd ..
+  mkdir -p build/$BASENAME
+  mv *.deb build/$BASENAME
+  mv *.build build/$BASENAME
+  mv *.buildinfo build/$BASENAME
+  mv *.changes build/$BASENAME
+done
+chown -R user .
