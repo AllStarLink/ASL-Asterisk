e7017e905ddf3a5a5ea027f30d849a21f10e1285
Adam Paul <adamjpaul@gmail.com>
2022-12-27 21:33:36 +0000

    remove unnecessary environment variable

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -65,7 +65,7 @@ fi
 DPKG_BUILDOPTS="-b -uc -us"
 for A in $ARCHS; do
        docker build -f $DIR/Dockerfile.$A -t asl-asterisk_builder.$A --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) $DIR
-       docker run -v $PDIR:/src -e DPKG_BUILDOPTS="$DPKG_BUILDOPTS" -e BUILD_TARGETS="$BUILD_TARGETS" -e FOO=bar asl-asterisk_builder.$A
+       docker run -v $PDIR:/src -e DPKG_BUILDOPTS="$DPKG_BUILDOPTS" -e BUILD_TARGETS="$BUILD_TARGETS" asl-asterisk_builder.$A
        docker image rm --force asl-asterisk_builder.$A
        DPKG_BUILDOPTS="--build=any -uc -us"
 done
